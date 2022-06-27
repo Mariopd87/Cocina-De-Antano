a2f5f5a2630dec9901aaee32991cbc74d2bebb25
Mariopd87 <mariopd87@gmail.com>
2022-06-27 22:31:44 +0200

    Remove console log for DB Connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,10 +3,7 @@ module.exports = (mongoose) => {
 
   mongoose
     .connect(urlDB)
-    .then(() => {
-      console.log("CONECTADO A MONGO DB");
-      console.log("Escuchando en el puerto 8000");
-    })
+    .then()
     .catch((error) => console.error(error));
 
   var productosSchema = mongoose.Schema(
